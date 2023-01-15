68a74b22d7ac7d3926e03ee4901f6e26d0a48005
서민성 <87956613+minseong0609@users.noreply.github.com>
2023-01-16 01:12:12 +0900

    [Silver V] Title: 배열 합치기, Time: 1540 ms, Memory: 184028 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,25 +1,13 @@
-n,m = map(int, input().split())
+import sys
 
-a=[0]*n
-b=[0]*m
-result=[]
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+read = lambda: sys.stdin.readline().rstrip()
 
-check_a=0;
-check_b=0;
-check=0;
-while(check_a<n and check_b<m):
-    if(a[check_a]<=b[check_b]):
-        result.append(a[check_a])
-        check_a+=1
-    else:
-        result.append(b[check_b])
-        check_b+=1
+N, M = map(int, read().split())
 
-if(n>check_a):
-    result = result+a[check_a:n]
-else:
-    result = result+b[check_b:m]
+A = list(map(int, read().split()))
+B = list(map(int, read().split()))
 
-print(*result)
+answer = A + B
+answer.sort()
+
+print(*answer)
