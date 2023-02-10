78049d019fd5fe121b00a561cefa2279d2e4ec18
서민성 <87956613+minseong0609@users.noreply.github.com>
2023-02-10 18:21:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 248 ms, Memory: 117620 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-def DFS(graph, root):
+def dfs (graph, root):
     visited = []
     stack = [root]
 
@@ -19,7 +19,7 @@ def DFS(graph, root):
     return " ".join(str(i) for i in visited)
 
 
-def BFS(graph, root):
+def bfs (graph, root):
     visited = []
     queue = deque([root])
 
@@ -34,24 +34,21 @@ def BFS(graph, root):
     return " ".join(str(i) for i in visited)
 
 
+
+n, m, v = map(int, input().split())
 graph = {}
-n = input().split(' ')
+for i in range(m):
+    n1, n2 = map(int, input().split(' '))
 
-node, edge, start = [int(i) for i in n]
-for i in range(edge):
-    edge_info = input().split(' ')
-    n1, n2 = [int(j) for j in edge_info]
-    #print("graph ", graph)
-    if n1 not in graph:
+    if ( n1 not in graph ):
         graph[n1] = [n2]
-
     elif n2 not in graph[n1]:
         graph[n1].append(n2)
 
-    if n2 not in graph:
+    if ( n2 not in graph ):
         graph[n2] = [n1]
-    elif n1 not in graph[n2]:
+    elif (n1 not in graph[n2]):
         graph[n2].append(n1)
 
-print(DFS(graph, start))
-print(BFS(graph, start))
+print(dfs(graph, v))
+print(bfs(graph, v))
