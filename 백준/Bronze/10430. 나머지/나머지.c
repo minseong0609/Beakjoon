18f298bc265676f701002419b7c0f320bbcf85e8
서민성 <87956613+minseong0609@users.noreply.github.com>
2024-01-02 23:40:13 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a, b,c=0;
+	scanf("%d %d %d", &a,&b,&c);
+	printf("%d\n", (a+b)%c);
+	printf("%d\n", ((a%c)+(b%c))%c );
+	printf("%d\n", (a*b)%c);
+	printf("%d", ((a%c)*(b%c))%c);
+}
